3d201c4a42e48cd624bb372b499d490eece5d992
Jeffrey Vervoort <jeffrey.vervoort@knmi.nl>
2025-09-23 15:29:47 +0200

    Change uvicorn settings for debugging.

--- main.py
+++ main.py
@@ -114,4 +114,4 @@ async def get_collection_metadata(request: Request) -> Collection:
 app.include_router(observations.router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
